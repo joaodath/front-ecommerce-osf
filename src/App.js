0ab0d6d906414f49aecb2e4dd8a60f5f4ca58083
Flavio Wu <flaviowu@gmail.com>
2021-10-28 20:25:11 -0300

    build: add react-router-dom & initial pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.scss';
+import { Switch, Route } from "react-router-dom";
+import Home from './pages/Home/Home';
+import Header from "./components/shared/Header/Header";
+import Footer from "./components/shared/Footer/Footer"
 
 function App() {
   return (
     <div className="App">
-
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+      </Switch>
+      <Footer/>
     </div>
   );
 }
