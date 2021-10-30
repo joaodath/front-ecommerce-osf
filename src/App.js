3e9711f22efb1ef9228e6a11b21af862d9178e53
Claudenir Ferreira <claudenir_fer@yahoo.com.br>
2021-10-29 21:16:07 -0300

    feat: Routes Module

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Home from './pages/Home/Home';
 import Header from "./components/shared/Header/Header";
 import Footer from "./components/shared/Footer/Footer"
+import { RoutesComponents } from './routes/RoutesComponents'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Switch>
         <Route exact path="/" component={Home}/>
+        <RoutesComponents />
       </Switch>
       <Footer/>
     </div>
---
+++ src/routes/RoutesComponents.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route } from "react-router";
+// import { Book } from "../components/Book/CreateBook";
+
+export const RoutesComponents = () => {
+  return (
+    <>
+      {/* <Route path="book/create" components={CreateBook} /> */}
+    </>
+  );
+};
