a2befefc641324585db4b76361f88e3b4d612cf5
Elias Santana <eliassilva0045@gmail.com>
2021-11-09 22:27:43 -0300

    feat(produtos) add icons to the cards

--- src/components/structure/BookCard/BookCard.jsx
+++ src/components/structure/BookCard/BookCard.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, {useState}from 'react'
 import { Box, Typography, Card, CardContent, CardMedia, CardActions, Button} from '@mui/material'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 export default function BookCard(props) {
-
+    const [isFavorite, setIsFavorite] = useState(false)
+    const favoritar = () => {
+        if(!isFavorite){
+            setIsFavorite(true)
+        }else{
+            setIsFavorite(false)
+        }
+    }
     return (
         <Box
         display="flex"
         flexDirection="column"
-        style={{height: '100%', backgroundColor: 'red'}}
+        style={{height: '100%', backgroundColor: 'gray'}}
     >
         <Card sx={{ maxWidth: 360 }}>
             <CardMedia
@@ -24,17 +32,12 @@ export default function BookCard(props) {
                 <Typography variant="body2" color="text.secondary">
                 {props.data.price}
                 </Typography>
+                <CardActions >
+                    <Button size="small"><ShareIcon/></Button>
+                    <Button size="small" onClick={favoritar}>{isFavorite? <FavoriteIcon/>:<FavoriteBorderIcon/>}</Button>
+                </CardActions>
             </CardContent>
-            <CardActions 
-                container
-                direction="row"
-                justify="space-evenly"
-                alignItems="stretch"
-                spacing={2}
-            >
-                <Button size="small"><ShareIcon/></Button>
-                <Button size="small"><FavoriteBorderIcon/></Button>
-            </CardActions>
+            
         </Card>
     </Box>
     )
