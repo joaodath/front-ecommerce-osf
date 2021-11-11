8c9a48ac80f3e8ea18b9d4c100a6965d79fdc516
Claudenir Ferreira <claudenir_fer@yahoo.com.br>
2021-11-10 22:03:19 -0300

    feat: Api

--- src/routes/RoutesComponents.js
+++ src/routes/RoutesComponents.js
@@ -1,11 +1,23 @@
+// import { Switch } from "@mui/material";
 import React from "react";
-import { Route } from "react-router";
+import { Switch, Route } from "react-router-dom";
+import CreateUser from "../pages/CreateUser/CreateUser";
+import DeleteUser from "../pages/DeleteUser/DeleteUser";
+import UpdateUser from "../pages/UpdateUser/UpdateUser";
+import ViewUser from "../pages/ViewUser/ViewUser";
+import ViewUserName from "../pages/ViewUserName/ViewUserName";
+
 // import { Book } from "../components/Book/CreateBook";
 
 export const RoutesComponents = () => {
   return (
     <>
-      {/* <Route path="book/create" components={CreateBook} /> */}
+      <Route path="/user/create" component={CreateUser} />
+
+      <Route path="/user/view/username" component={ViewUserName} />
+      <Route path="/user/view/id" component={ViewUser} />
+      <Route path="/user/update" component={UpdateUser} />
+      <Route path="/user/delete" component={DeleteUser} />
     </>
   );
 };
