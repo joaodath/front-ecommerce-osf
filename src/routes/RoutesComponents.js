e6d585e1c04e663ccd0628003d9be9c51ed11ebc
Claudenir Ferreira <63820492+ClaudenirFer@users.noreply.github.com>
2021-11-15 11:16:59 -0300

    feat: Adicionado novas rotas e alterando as existentes p/ receber parâmetros

--- src/routes/RoutesComponents.js
+++ src/routes/RoutesComponents.js
@@ -1,23 +1,34 @@
-// import { Switch } from "@mui/material";
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Route } from "react-router";
+import { Home } from "../pages/Home/Home";
 import CreateUser from "../pages/CreateUser/CreateUser";
 import DeleteUser from "../pages/DeleteUser/DeleteUser";
 import UpdateUser from "../pages/UpdateUser/UpdateUser";
 import ViewUser from "../pages/ViewUser/ViewUser";
 import ViewUserName from "../pages/ViewUserName/ViewUserName";
-
+import Login from "../pages/Login/Login";
+import Logout from "../pages/Logout/Logout";
+// import Books from "../pages/Books/Books";
+// import ViewCart from "../pages/ViewCart/ViewCart";
 // import { Book } from "../components/Book/CreateBook";
 
 export const RoutesComponents = () => {
   return (
     <>
-      <Route path="/user/create" component={CreateUser} />
+      {/* <Route path="book/create" components={CreateBook} /> */}
+      {/* <Route path="/books" component={Books}/>  */}
+      {/* <Route path="/cart" component={ViewCart}/>  */}
 
-      <Route path="/user/view/username" component={ViewUserName} />
-      <Route path="/user/view/id" component={ViewUser} />
-      <Route path="/user/update" component={UpdateUser} />
-      <Route path="/user/delete" component={DeleteUser} />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/logout" component={Logout} />
+
+
+      <Route path="/user/create" component={CreateUser} />
+      <Route path="/user/update/:id" component={UpdateUser} />
+      <Route path="/user/view/id/:id" component={ViewUser} />
+      <Route path="/user/view/username/:username" component={ViewUserName} />
+      <Route path="/user/delete/:id" component={DeleteUser} />
     </>
   );
 };
