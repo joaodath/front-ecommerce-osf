b68aebd7c70b885d5584817b2d78e8db127821ce
Claudenir Ferreira <63820492+ClaudenirFer@users.noreply.github.com>
2021-11-15 11:01:02 -0300

    feat: init Header

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Api } from "../../../Api/Api";
+import { JwtHandler } from "../../../jwt-handler/JwtHandler";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import "./Header.css";
 
-function Header() {
-    return (
-        <div>
-            <h3>Header</h3>
-        </div>
-    )
+export default function Header(props) {
+  const history = useHistory();
+  const [isLogged, setIsLogged] = useState(JwtHandler.isJwtValid);
+
+  useEffect(() => {
+    const handleOnJwtChange = () => {
+      setIsLogged(JwtHandler.isJwtValid());
+    };
+    window.addEventListener("onJwtChange", handleOnJwtChange);
+    return () => {
+      window.removeEventListener("onJwtChange", handleOnJwtChange);
+    };
+  }, []);
+
+  console.log("IsLogged", JwtHandler.getJwt());
+
+  // const body = undefined;
+  const pass = JwtHandler.getJwt();
+
+  const loadUser = async () => {
+    const response = await Api.buildApiGetRequest(Api.loginTestUrl(pass), true);
+
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      history.push("/logout");
+    } else {
+    }
+  };
+
+  loadUser();
+
+  return (
+    <header className="header">
+      <br />
+      <Link to="/">Home</Link>
+      <br />
+
+      {isLogged ? (
+        <Link to="/logout">Logout</Link>
+      ) : (
+        <Link to="/login">Login</Link>
+      )}
+      <br />
+      <br />
+    </header>
+  );
 }
-
-export default Header
