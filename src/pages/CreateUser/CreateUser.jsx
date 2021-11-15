b874072a4be8711905e80bc93ba5b6b24859efb6
Claudenir Ferreira <63820492+ClaudenirFer@users.noreply.github.com>
2021-11-15 11:03:53 -0300

    refactor: init: Formulário material ui

--- src/pages/CreateUser/CreateUser.jsx
+++ src/pages/CreateUser/CreateUser.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { Api } from "../../api/Api";
+import { Api } from "../../Api/Api";
+import Box from "@mui/material/Box";
+import { Grid } from "@material-ui/core";
+import TextField from "@mui/material/TextField";
+import './createuser.css'
 
 export default function CreateUser(props) {
   // div*12>label.form__label>input.form__input-text>p
@@ -49,224 +53,257 @@ export default function CreateUser(props) {
     if (response.status === 201) {
       alert("Cadastro realizado com sucesso");
       const id = body.id;
-      
-      // Verifique seus dados
-      //props.history.push(`/user/view/${id}`);
-      
+      props.history.push(`/user/view/id/${id}`);
     } else {
       alert("Ops! Algo deu errado!");
     }
   };
 
   return (
-    <div>
-      <form className="form" onSubmit={handleSubmit}>
+    <div className="App">
+      <div>
         <div>
+          <h1>Cadastre-se</h1>
+        </div>
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          {/* <form className="form" onSubmit={handleSubmit}> */}
+          {/* <div>
           <label className="form__label" htmlFor="name">
-            Nome:
+          Nome:
           </label>
-        </div>
+        </div> */}
+          <Grid container>
+            <Grid item xs={12}>
+              <div>
+                <TextField
+                  required
+                  type="text"
+                  className="outlined-helperText"
+                  label="NOME COMLETO"
+                  name="name"
+                  id="name"
+                />
+              </div>
 
-        <div>
-          <input
-            type="text"
-            className="form__input-text"
-            name="name"
-            id="name"
-          />
-        </div>
+              {/* <div>
+              <label className="form__label" htmlFor="username">
+                Nome de Usuário:
+                </label>
+              </div> */}
 
-        <div>
-          <label className="form__label" htmlFor="username">
-            Nome de Usuário:
-          </label>
-        </div>
+              <div>
+                <TextField
+                  required
+                  type="text"
+                  className="outlined-helperText"
+                  label="NOME DE USUÁRIO"
+                  name="username"
+                  id="username"
+                  helperText="Apenas o nome de usuário."
+                />
+              </div>
 
-        <div>
-          <input
-            type="text"
-            className="form__input-text"
-            name="username"
-            id="username"
-          />
-        </div>
+              {/* <div>
+              <label htmlFor="email" className="form__label">
+              Email:
+              </label>
+            </div> */}
 
-        <div>
-          <label htmlFor="email" className="form__label">
-            Email:
-          </label>
-        </div>
+              <div>
+                <TextField
+                  type="text"
+                  className="outlined-helperText"
+                  label="EMAIL"
+                  name="email"
+                  id="email"
+                />
+              </div>
 
-        <div>
-          <input
-            type="text"
-            className="form__input-text"
-            name="email"
-            id="email"
-          />
-        </div>
+              {/* <div>
+              <label htmlFor="password" className="form__label">
+              Senha:
+              </label>
+            </div> */}
 
-        <div>
-          <label htmlFor="password" className="form__label">
-            Senha:
-          </label>
-        </div>
+              <div>
+                <TextField
+                  type="password"
+                  className="outlined-password-input"
+                  label="SENHA"
+                  name="password"
+                  id="password"
+                  helperText="Mínimo de 8 caracteres"
+                />
+              </div>
 
-        <div>
-          <input
-            type="password"
-            className="form__input-text"
-            name="password"
-            id="password"
-          />
-        </div>
+              {/* <div>
+              <label htmlFor="profilePhoto" className="form__label">
+                Foto de Perfil:
+              </label>
+            </div> */}
 
-        <div>
-          <label htmlFor="profilePhoto" className="form__label">
-            Foto de Perfil:
-          </label>
-        </div>
+              <div>
+                <TextField
+                  type="text"
+                  className="outlined-helperText"
+                  label="IMAGEM DE PERFIL"
+                  name="profilePhoto"
+                  id="profilePhoto"
+                  helperText="Insira o link da imagem"
+                />
+              </div>
 
-        <div>
-          <input
-            type="text"
-            className="form__input-text"
-            name="profilePhoto"
-            id="profilePhoto"
-          />
-        </div>
+              {/* <div>
+              <label htmlFor="birthDate" className="form__label">
+              Data de Nascimento:
+              </label>
+            </div> */}
 
-        <div>
-          <label htmlFor="birthDate" className="form__label">
-            Data de Nascimento:
-          </label>
-        </div>
+              <div>
+                <TextField
+                  type="text"
+                  className="outlined-helperText"
+                  label="Data de Nascimento"
+                  name="birthDate"
+                  id="birthDate"
+                  helperText="Formato: DD/MM/YYYY"
+                />
+              </div>
+            </Grid>
+            <Grid item xs={12}>
+              {/* <div>
+              <label htmlFor="cpf" className="form__label">
+              CPF:
+              </label>
+            </div> */}
 
-        <div>
-          <input
-            type="text"
-            className="form__input-text"
-            name="birthDate"
-            id="birthDate"
-          />
-        </div>
-        <div>
-          <label htmlFor="cpf" className="form__label">
-            CPF:
-          </label>
-        </div>
+              <div>
+                <TextField
+                  type="text"
+                  className="outlined-helperText"
+                  label="CPF"
+                  name="cpf"
+                  id="cpf"
+                />
+              </div>
 
-        <div>
-          <input type="text" className="form__input-text" name="cpf" id="cpf" />
-        </div>
+              {/* <div>
+              <label htmlFor="cep" className="form__label">
+              CEP:
+              </label>
+            </div> */}
 
-        <div>
-          <label htmlFor="cep" className="form__label">
-            CEP:
-          </label>
-        </div>
+              <div>
+                <TextField
+                  type="text"
+                  className="outlined-helperText"
+                  label="CEP"
+                  name="cep"
+                  id="cep"
+                />
+              </div>
 
-        <div>
-          <input type="text" className="form__input-text" name="cep" id="cep" />
-        </div>
+              {/* <div>
+              <label htmlFor="country" className="form__label">
+              País:
+              </label>
+            </div> */}
 
-        <div>
-          <label htmlFor="country" className="form__label">
-            País:
-          </label>
-        </div>
+              <div>
+                <TextField
+                  type="text"
+                  className="outlined-helperText"
+                  label="PAÍS"
+                  name="country"
+                  id="country"
+                />
+              </div>
 
-        <div>
-          <input
-            type="text"
-            className="form__input-text"
-            name="country"
-            id="country"
-          />
-        </div>
+              {/* <div>
+              <label htmlFor="state" className="form__label">
+              Estado:
+              </label>
+            </div> */}
 
-        <div>
-          <label htmlFor="state" className="form__label">
-            Estado:
-          </label>
-        </div>
+              <div>
+                <TextField
+                  type="text"
+                  className="outlined-helperText"
+                  label="ESTADO"
+                  name="state"
+                  id="state"
+                />
+              </div>
 
-        <div>
-          <input
-            type="text"
-            className="form__input-text"
-            name="state"
-            id="state"
-          />
-        </div>
+              {/* <div>
+              <label htmlFor="city" className="form__label">
+                Cidade:
+                </label>
+            </div> */}
 
-        <div>
-          <label htmlFor="city" className="form__label">
-            Cidade:
-          </label>
-        </div>
+              <div>
+                <TextField
+                  type="text"
+                  className="outlined-helperText"
+                  label="CIDADE"
+                  name="city"
+                  id="city"
+                />
+              </div>
 
-        <div>
-          <input
-            type="text"
-            className="form__input-text"
-            name="city"
-            id="city"
-          />
-        </div>
+              {/* <div>
+              <label htmlFor="address" className="form__label">
+                Endereço:
+                </label>
+            </div> */}
 
-        <div>
-          <label htmlFor="address" className="form__label">
-            Endereço:
-          </label>
-        </div>
+              <div>
+                <TextField
+                  type="text"
+                  className="filled-textarea"
+                  label="ENDEREÇO"
+                  multiline
+                  name="address"
+                  id="address"
+                  maxRows={4}
+                  helperText="Rua, número, complemento e Bairro "
+                />
+              </div>
 
-        <div>
-          <input
-            type="text"
-            className="form__input-text"
-            name="address"
-            id="address"
-          />
-        </div>
+              {/* <div>
+              <label htmlFor="phonenumber" className="form__label">
+                Telefone:
+                </label>
+              </div> */}
 
-        <div>
-          <label htmlFor="phonenumber" className="form__label">
-            Telefone:
-          </label>
-        </div>
+              <div>
+                <TextField
+                  type="text"
+                  className="outlined-helperText"
+                  label="TELEFONE"
+                  name="phonenumber"
+                  id="phonenumber"
+                />
+              </div>
+            </Grid>
 
-        <div>
-          <input
-            type="text"
-            className="form__input-text"
-            name="phonenumber"
-            id="phonenumber"
-          />
-        </div>
-
-        <div>
-          <input className="form__submit" type="submit" value="CRIAR" />
-        </div>
-      </form>
+            <Grid item xs={12}>
+              <div>
+                <TextField className="contained" type="submit" value="CRIAR" />
+              </div>
+            </Grid>
+            {/* </form> */}
+          </Grid>
+        </Box>
+      </div>
     </div>
   );
 }
-
-// {
-
-//   "name": "Paulo",
-//   "username": "Moreira Filho",
-//   "email": "paulo@gmail.com",
-//   "password": "Ab123@",
-//   "profilePhoto": "pl22",
-//   "birthDate": "2016-07-16T19:20:30+05:30",
-//   "cpf": "11221122112",
-//   "cep": "12312312",
-//   "country": "Brasil",
-//   "state": "Minas Gerais",
-//   "city": "Piraúba",
-//   "address": "Rua do Ouvidor, 21 - Mineira",
-//   "phonenumber": "27978789696"
-
-// }
